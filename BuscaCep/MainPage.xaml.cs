5808903b3ed2d1980259c1f2dcca9a436c8d5096
Lucas Pinto <lucas.pinto@dxc.com>
2020-09-10 15:01:49 -0300

    adicionado aula 01 xamarin

---
+++ BuscaCep/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuscaCep.Servicos;
+//using BuscaCep.Servicos.Modelo;
+using Xamarin.Forms;
+
+namespace BuscaCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BuscaCep(object sender, EventArgs args)
+        {
+            Endereco end = ServicosViaCep.BuscaEnderecoCep(CEP.Text.Trim());
+
+            Resultado.Text = String.Format("Endereço: {0} de {1}, {2} - {3}. {4}",
+                end.logradouro, end.bairro,end.localidade,end.uf,end.cep);
+            CEP.Text = "";
+        }
+    }
+}
---
+++ BuscaCep/Servicos/ServicosViaCep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BuscaCep.Servicos
+{
+
+
+    public class ServicosViaCep
+    {
+        private static String enderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public ServicosViaCep()
+        {
+        }
+
+        public static Endereco BuscaEnderecoCep(String cep)
+        {
+            String novoEnderecoURL = String.Format(enderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            String conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco ende = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return ende;
+        }
+    }
+}
